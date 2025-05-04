4683cc236f456eaeda01cf977c755b83fe74e37d
attarmau <attarmau111@gmail.com>
2025-05-04 11:17:57 +0800

    Update create_schema.js

--- init/create_schema.js
+++ init/create_schema.js
@@ -9,10 +9,24 @@ const vertexCollections = ["person", "call_event", "location"];
 vertexCollections.forEach((vc) => {
   if (!db._collection(vc)) {
     db._createDocumentCollection(vc);
+    print(`Created vertex collection: ${vc}`);
+  } else {
+    print(`Vertex collection already exists: ${vc}`);
   }
 });
 
 // 2. Create Edge Collections
+const edgeCollections = ["called", "made_at"];
+edgeCollections.forEach((ec) => {
+  if (!db._collection(ec)) {
+    db._createEdgeCollection(ec);
+    print(`Created edge collection: ${ec}`);
+  } else {
+    print(`Edge collection already exists: ${ec}`);
+  }
+});
+
+// 3. Define Edge Relations for the Graph
 const edgeDefinitions = [
   {
     collection: "called",
@@ -26,18 +40,14 @@ const edgeDefinitions = [
   },
 ];
 
-// Create edge collections if not exist
-edgeDefinitions.forEach((edge) => {
-  if (!db._collection(edge.collection)) {
-    db._createEdgeCollection(edge.collection);
-  }
-});
-
-// 3. Create Graph
+// 4. Create the Graph
 if (!graph_module._exists(GRAPH_NAME)) {
   graph_module._create(
     GRAPH_NAME,
     edgeDefinitions,
     []
   );
+  print(`Graph '${GRAPH_NAME}' created.`);
+} else {
+  print(`Graph '${GRAPH_NAME}' already exists.`);
 }
